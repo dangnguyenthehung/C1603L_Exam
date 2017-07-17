5dc7978443d51e709832ea138469833bae8091f2
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2017-07-18 00:26:27 +0700

    init app

---
+++ C1603L_Exam/Helper.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+
+namespace C1603L_Exam
+{
+    public class Helper
+    {
+        public static string GetImageName()
+        {
+            return 
+                $"{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}{DateTime.Now.Millisecond}";
+        }
+
+        public static async Task<SoftwareBitmapSource> ConvertToBitMap(IRandomAccessStream stream)
+        {
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Premultiplied);
+
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+            return bitmapSource;
+        }
+
+        public static async Task<StorageFile> ApplyBlurEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var blur = new GaussianBlurEffect();
+                blur.BlurAmount = 9.0f;
+                blur.BorderMode = EffectBorderMode.Hard;
+                blur.Optimization = EffectOptimization.Quality;
+                blur.Source = bitmap;
+
+                ds.DrawImage(blur);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyHueRotationEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var hue = new HueRotationEffect
+                {
+                    Angle = (float)Math.PI / 2,
+                    Source = bitmap
+                };
+                ds.DrawImage(hue);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplySaturationEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new SaturationEffect()
+                {
+                    Saturation = (float) 0.3,
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyLinearTransferEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new LinearTransferEffect()
+                {
+                    AlphaOffset = (float)0.5,
+                    BlueOffset = (float)0.2,
+                    GreenSlope = (float)0.3,
+                    RedOffset = (float)0.3,
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyContrastEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new ContrastEffect()
+                {
+                    Contrast = (float)-0.8,
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyExposureEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new ExposureEffect()
+                {
+                    Exposure = (float)1.2,
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyGammaEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new GammaTransferEffect()
+                {
+                    AlphaAmplitude = (float)0.9,
+                    BlueAmplitude = (float)0.8,
+                    GreenAmplitude = (float)0.9,
+                    RedAmplitude = (float)0.8,
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> ApplyGrayscaleEffect(IRandomAccessStream stream)
+        {
+            var device = new CanvasDevice();
+            var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+            var renderer = new CanvasRenderTarget(device, bitmap.SizeInPixels.Width, bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+            using (var ds = renderer.CreateDrawingSession())
+            {
+                var saturation = new GrayscaleEffect()
+                {
+                    Source = bitmap
+                };
+                ds.DrawImage(saturation);
+            }
+
+            var saveFile = await SaveTempoaryFile(renderer);
+
+            return saveFile;
+        }
+
+        public static async Task<StorageFile> SaveTempoaryFile(CanvasRenderTarget renderer)
+        {
+            StorageFolder destinationFolder =
+                await ApplicationData.Current.LocalFolder.CreateFolderAsync("CapturePhoto",
+                    CreationCollisionOption.OpenIfExists);
+
+            var saveFile = await destinationFolder.CreateFileAsync("temp.jpg", CreationCollisionOption.ReplaceExisting);
+
+            using (var outStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                await renderer.SaveAsync(outStream, CanvasBitmapFileFormat.Jpeg);
+            }
+
+            return saveFile;
+        }
+    }
+}
---
+++ C1603L_Exam/MainPage.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace C1603L_Exam
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string ImagePath = "";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btn_CaptureImage_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI captureUI = new CameraCaptureUI();
+            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+
+            StorageFile photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (photo == null)
+            {
+                // User cancelled photo capture
+                return;
+            }
+
+            var fileName = $"{Helper.GetImageName()}.jpg";
+            //var saveFolder = "D://CapturePhoto";
+            //if (!Directory.Exists(saveFolder))
+            //{
+            //    Directory.CreateDirectory(saveFolder);
+            //}
+            //StorageFolder destinationFolder = await StorageFolder.GetFolderFromPathAsync(saveFolder);
+
+
+            StorageFolder destinationFolder =
+                await ApplicationData.Current.LocalFolder.CreateFolderAsync("CapturePhoto",
+                    CreationCollisionOption.OpenIfExists);
+
+            await photo.CopyAsync(destinationFolder, fileName, NameCollisionOption.ReplaceExisting);
+
+            ImagePath = $"{destinationFolder.Path}\\{fileName}";
+
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            
+            ImageFrame.Source = await Helper.ConvertToBitMap(stream);
+        }
+
+        private async void btn_ApplyFilter_Blur_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyBlurEffect(stream);
+                
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Hue_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyHueRotationEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Saturation_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplySaturationEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_LinearTransfer_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyLinearTransferEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Contrast_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyContrastEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Exposure_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyExposureEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Gamma_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyGammaEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+
+        private async void btn_ApplyFilter_Grayscale_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromPathAsync(ImagePath);
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                var saveFile = await Helper.ApplyGrayscaleEffect(stream);
+
+                // Display Image
+                using (var inputStream = await saveFile.OpenAsync(FileAccessMode.Read))
+                {
+                    ImageFrame_Filter.Source = await Helper.ConvertToBitMap(inputStream);
+                }
+            }
+        }
+    }
+}
